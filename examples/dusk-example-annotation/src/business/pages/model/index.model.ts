1b6b5024e4f7e19c3c37ca75c4d5ccb8ca4bfb3a
xams-creator <1228552328@qq.com>
2021-11-06 15:09:52 +0800

    examples: type测试

--- examples/dusk-example-annotation/src/business/pages/model/index.model.ts
+++ examples/dusk-example-annotation/src/business/pages/model/index.model.ts
@@ -7,21 +7,22 @@ interface State {
 /*
    todo? : 后续增加类模式model，可以做继承通用化,
             model actions,reducers 增加具体类型
-            model内部操作时的帮助方法
+            model actions内部操作时的帮助方法
+            model 自动生成type常量
             class model 增加 @effect
 **/
 
-const model: Model = {
+const model: Model<State> = {
     namespace: 'model',
     state: {
         value: 0,
     },
     scoped: {
         reducers: {
-            add(state: State) {
+            add(state) {
                 state.value += 1;
             },
-            minus(state: State) {
+            minus(state) {
                 state.value -= 1;
             },
         },
