2b74c6d535357f43a2032f6f76b2a6adae8fab1a
xams-creator <1228552328@qq.com>
2023-01-06 15:15:40 +0800

    upgrade: 1.0.4

--- packages/dusk-plugin-hmr/src/index.ts
+++ packages/dusk-plugin-hmr/src/index.ts
@@ -1,17 +1,21 @@
 import Dusk, { PluginFunction, PluginHookContext, isProduction } from '@xams-framework/dusk';
-
-/*
-*   todo：目前版本hook name 为 string，在这种情况下可能不安全，因为可以通过 emit(string) 破坏规则.
-*    后面可以尝试使用 Symbol.for(key),Symbol.keyFor(symbol)
-* **/
-
 // const APP_HOOKS_ON_HMR = Symbol.for('onHmr');
 const APP_HOOKS_ON_HMR = 'onHmr';
 Dusk.configuration.plugin.hooks.push(APP_HOOKS_ON_HMR);
 
 declare module '@xams-framework/dusk' {
+    interface DuskApplication {
+        $hmr: any;
+    }
+
     interface Plugin {
         onHmr?: (ctx: PluginHookContext, next: Function) => void;
+    }
+}
+
+declare global {
+    interface Window {
+        __DUSK_PLUGIN_HMR_STATUS_HANDLER: Function;
     }
 }
 
@@ -19,46 +23,66 @@ type Status = 'check' | 'prepare' | 'dispose' | 'apply' | 'idle';
 
 declare const module: {
     hot: {
-        addStatusHandler: (status) => void
+        addStatusHandler: (status) => Function
+        removeStatusHandler: (callback: Function) => void
     }
 };
 
 
-function supportHMR() {
-    // @ts-ignore
-    if (module.hot) {
-        // @ts-ignore
-        if (module.hot.status() === 'idle') {
-            return true;
-        }
-    }
-    return false;
+// function supportHMR() {
+//     // @ts-ignore
+//     if (module.hot) {
+//         // @ts-ignore
+//         if (module.hot.status() === 'idle') {
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+interface DuskHmrOptions {
+    disposeApp?: boolean;
 }
 
-export default function createDuskHmr(options?: any): PluginFunction {
+export default function createDuskHmr(options: DuskHmrOptions = {
+    disposeApp: false,
+}): PluginFunction {
     return (app) => {
         return {
             name: 'dusk-plugin-hmr',
             setup() {
-                if (!isProduction() && module.hot && !Dusk.configuration.hmr) {
-                    Dusk.configuration.hmr = true;
-                    module.hot.addStatusHandler((status: Status) => {
-                        switch (status) {
-                            case 'check':
-                            case 'prepare':
-                                break;
-                            case 'dispose':
-                                app.destroy();
-                                break;
-                            case 'apply':
-                                break;
-                            case 'idle':
-                                app._pm.apply(APP_HOOKS_ON_HMR);
-                                break;
-                            default:
-                                break;
-                        }
-                    });
+                if (!isProduction() && module.hot) {
+                    if (!Dusk.configuration.hmr) {
+                        Dusk.configuration.hmr = true;
+                        module.hot.removeStatusHandler(window.__DUSK_PLUGIN_HMR_STATUS_HANDLER);
+                        window.__DUSK_PLUGIN_HMR_STATUS_HANDLER = (status) => {
+                            switch (status) {
+                                case 'check':
+                                case 'prepare':
+                                    break;
+                                case 'dispose':
+                                    // if (options.disposeApp) {
+                                    //     app.destroy();
+                                    // } else {
+                                    //     app._pm.dispose();
+                                    //     app._mm.dispose();
+                                    //     // @ts-ignore
+                                    //     app._cm.dispose();
+                                    // }
+                                    break;
+                                case 'apply':
+                                    break;
+                                case 'idle':
+                                    setTimeout(() => {
+                                        app._pm.apply(APP_HOOKS_ON_HMR);
+                                    });
+                                    break;
+                                default:
+                                    break;
+                            }
+                        };
+                        module.hot.addStatusHandler(window.__DUSK_PLUGIN_HMR_STATUS_HANDLER);
+                    }
                 }
             },
         };
