efb91472d65749e9603a80ff5b1dad6e7aec1cfc
xams-creator <1228552328@qq.com>
2023-05-01 02:14:27 +0800

    upgrade: 0.0.2 dusk-plugin-context

--- packages/dusk-plugin-context/src/dusk-plugin-context-webpack.ts
+++ packages/dusk-plugin-context/src/dusk-plugin-context-webpack.ts
@@ -1,11 +1,24 @@
-import { definePlugin } from '@xams-framework/dusk';
-import { DuskContextOptions } from './index';
+import { definePlugin, DuskModel } from '@xams-framework/dusk';
+import { DuskContextOptions, WebpackContext } from './index';
 
-export default function createDuskContextWebpack(options: DuskContextOptions = {}) {
+export default function createDuskContextWebpack({ context }: DuskContextOptions) {
     return definePlugin({
         name: 'dusk-plugin-context-webpack',
         setup(app) {
+            const ctx: WebpackContext =
+                context ||
+                // @ts-ignore
+                require.context((process.env.REACT_APP_PATH_SRC_ALIAS_NAME || 'src') + '/business/inject',
+                    true,
+                    /\.(tsx|ts|js|jsx)$/,
+                );
 
+            ctx.keys().forEach(key => {
+                const model: DuskModel = ctx(key).default;
+                if (model && !!model.reducer) {
+                    app.define(model);
+                }
+            });
         },
     });
 }
--- packages/dusk-plugin-context/src/index.tsx
+++ packages/dusk-plugin-context/src/index.tsx
@@ -1,76 +1,56 @@
-import Dusk, { definePlugin, DuskModel, useDusk, DynamicComponentProps } from '@xams-framework/dusk';
+import Dusk, { definePlugin } from '@xams-framework/dusk';
 import { inWebpack } from '@xams-framework/common';
 import * as React from 'react';
+import createDuskContextWebpack from './dusk-plugin-context-webpack';
 
+export type WebpackContext = ((id: string) => any) & {
+    keys(): string[];
+};
 
 export interface DuskContextOptions {
-
-    models?: string;
+    context?: WebpackContext;
 }
 
-export default function createDuskContext(options?: DuskContextOptions) {
-
+export default function createDuskContext(options: DuskContextOptions) {
     return definePlugin({
         name: 'dusk-plugin-context',
         setup(app) {
             if (Dusk.configuration.experimental.context) {
-                // @ts-ignore
-                // let modules = require.context(
-                //     (process.env.REACT_APP_PATH_SRC_ALIAS_NAME || 'src') + '/business',
-                //     true,
-                //     /\.(tsx|ts|js|jsx)$/
-                // );
-                // modules.keys().forEach(key => {
-                //     modules(key);
-                // });
                 if (inWebpack()) {
-                    // @ts-ignore
-                    let modules = require.context(
-                        // @ts-ignore
-                        (process.env.REACT_APP_PATH_SRC_ALIAS_NAME || 'src') + '/business/models',
-                        true,
-                        /model\.(tsx|ts|js|jsx)$/,
-                    );
-
-                    modules.keys().forEach(key => {
-                        const model: DuskModel = modules(key).default;
-                        if (model && !!model.reducer) {
-                            app.define(model as any);
-                        }
-                    });
+                    app.use(createDuskContextWebpack(options));
                 }
             }
         },
     });
 }
 
-export function useDynamicComponent(props: DynamicComponentProps) {
-    const app = useDusk();
-    const id = normalizeDotRule(props.id || props.typeId as string);
-    let res;
-    try {
-        res = app._cm.components[id];
-        if (!res) {
-            // @ts-ignore
-            res = require(`${(process.env.REACT_APP_PATH_SRC_ALIAS_NAME || 'src')}/${id}`);
-            // const v = import(`@/${id}`)
-        }
-    } catch (e) {
-        app.$logger.warn(`${e}, will use Dusk.configuration.suspense.renderLoading`);
-        // throw e;
-        return [() => {
-            return (Dusk.configuration.suspense.renderLoading);
-        }];
-        // throw e;
-    }
-    return [res.default, res];
-}
-
-function normalizeDotRule(searchValue: string, replaceValue = '/'): string {
-    return searchValue.replace(/\./g, replaceValue);
-}
-
-export function DynamicComponent(props: DynamicComponentProps) {
-    const [Component] = useDynamicComponent(props);
-    return (<Component {...props.props} />);
-}
+// export function useDynamicComponent(props: DynamicComponentProps) {
+//     const app = useDusk();
+//     const id = normalizeDotRule(props.id || props.typeId as string);
+//     let res;
+//     try {
+//         res = app._cm.components[id];
+//         if (!res) {
+//             // @ts-ignore
+//             res = require(`${(process.env.REACT_APP_PATH_SRC_ALIAS_NAME || 'src')}/${id}`);
+//             // const v = import(`@/${id}`)
+//         }
+//     } catch (e) {
+//         app.$logger.warn(`${e}, will use Dusk.configuration.suspense.renderLoading`);
+//         // throw e;
+//         return [() => {
+//             return (Dusk.configuration.suspense.renderLoading);
+//         }];
+//         // throw e;
+//     }
+//     return [res.default, res];
+// }
+//
+// function normalizeDotRule(searchValue: string, replaceValue = '/'): string {
+//     return searchValue.replace(/\./g, replaceValue);
+// }
+//
+// export function DynamicComponent(props: DynamicComponentProps) {
+//     const [Component] = useDynamicComponent(props);
+//     return (<Component {...props.props} />);
+// }
